7d59c8990eae42a7f8c9f2509632309b45a1afc3
darkander <kingdarkander@gmail.com>
2021-08-27 10:51:47 +0300

    using less

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,20 @@ import "../styles/App.less";
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="header">
         <h1>My React App!</h1>
+        <button>hi</button>
+        <ul>
+          <li>Чебурашка</li>
+          <li>Крокодил Гена</li>
+          <li>Шапокляк</li>
+        </ul>
+        <div className="class">my name is Vika</div>
+        <a href="/"> visit it</a>
+        <div className="button">press me</div>
+        <div className="page">
+          <div className="val">ich liebe Deutch gern</div>
+        </div>
       </div>
     );
   }
